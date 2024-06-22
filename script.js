77297937e7160d0f6a665c0b93147767ef48e672
Jarckrios <jarckrios14@gmail.com>
2024-06-22 00:09:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+var phrases = [
+    "¿Estás segura?",
+    "¿Estás completamente segura?",
+    "Pero segura, segura",
+    "Por favor",
+    "Por favor, por favor",
+    "Ándale, perdóname",
+    "¡Ya perdóname!",
+    "No puede decir que no",
+    "Si dices que no eres enana",
+    "Enana, enana",
+    "Enana, enana, enana"
+];
+var currentPhrase = 0;
+var yesButton = document.getElementById('yesButton');
+var noButton = document.getElementById('noButton');
+
 document.getElementById('yesButton').addEventListener('click', function() {
     var container = document.querySelector('.container');
     container.innerHTML = `
         <h1>¿Quieres ser mi novia?</h1>
         <div class="buttons">
-            <button id="yesNewButton">Sí</button>
-            <button id="noNewButton">No</button>
+            <button id="yesNewButton" class="yes">Sí</button>
+            <button id="noNewButton" class="no">No</button>
         </div>
         <div id="newMessage"></div>
     `;
@@ -27,6 +44,14 @@ document.getElementById('yesButton').addEventListener('click', function() {
     document.getElementById('noNewButton').addEventListener('click', function(event) {
         event.preventDefault();
     });
+});
+
+document.getElementById('noButton').addEventListener('click', function() {
+    if (currentPhrase < phrases.length) {
+        document.getElementById('apologyMessage').textContent = phrases[currentPhrase];
+        currentPhrase++;
+        yesButton.style.transform = `scale(${1 + currentPhrase * 0.1})`;
+    }
 });
 
 document.getElementById('noButton').addEventListener('mouseover', function() {
